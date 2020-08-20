37c83fce36e2d79713d079f306acc593c830f50d
zeeshan-thedev <55945652+zeeshan-thedev@users.noreply.github.com>
2020-08-20 20:09:46 +0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,52 @@
+
+
+let stars = [];
+let speed;
+
+function setup() {
+
+	createCanvas(1300,650);
+	for (let i = 0; i < 800; i++) {
+		stars[i] = new Star();
+	}
+}
+
+
+
+
+function draw() {
+
+	speed = map(mouseX, 0, width, 0, 50)
+	background(0);
+	translate(width/2, height/2);
+
+	for (let i = 0; i < stars.length; i++) {
+		stars[i].update();
+		stars[i].show();
+			
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
